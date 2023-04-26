34597b5b41454cffc3c3998ff1ca19be92a49dec
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-04-26 12:29:43 +0530

    Handling Jwt Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -37,6 +37,24 @@ module.exports = (err, req, res, next) => {
             const message = `Duplicate ${Object.keys(err.keyValue)} entered`
             error = new ErrorHandler(message, 400)
         }
+
+
+        // Handling wrong JWT error
+        if (err.name === 'JsonWebTokenError') {
+            const message = 'JSON Web Token is invalid. Try Again!!!'
+            error = new ErrorHandler(message, 400)
+        }
+
+        // Handling Expired JWT error
+        if (err.name === 'TokenExpiredError') {
+            const message = 'JSON Web Token is expired. Try Again!!!'
+            error = new ErrorHandler(message, 400)
+        }
+
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        })
     }
 
 }
