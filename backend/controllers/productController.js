1d5ef84d55c9f9ca5479b249de9981cc4d63f370
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-04-26 11:59:29 +0530

    Add user Id In Product Schema

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,7 +28,7 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 
 exports.addNewProduct = catchAsyncError(async (req, res, next) => {
 
-
+    req.body.user = req.user.id;
     const product = await Product.create(req.body);
     res.status(200).json({
         success: true,
