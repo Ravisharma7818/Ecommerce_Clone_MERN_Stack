b0851520b39e53d7ca488a5c1f149d09e615ce0c
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-04-27 00:29:03 +0530

    Update And Delete Order By Admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,3 +86,49 @@ exports.allOrders = catchAsyncError(async (req, res, next) => {
         orders,
     });
 });
+
+
+
+// Update / Process order - ADMIN  =>   /api/v1/admin/order/:id
+exports.updateOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+
+    order.orderItems.forEach(async (item) => {
+        await updateStock(item.product, item.quantity);
+    });
+
+    (order.orderStatus = req.body.status), (order.deliveredAt = Date.now());
+
+    await order.save();
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+
+    product.stock = product.stock - quantity;
+
+    await product.save({ validateBeforeSave: false });
+}
+
+// Delete order   =>   /api/v1/admin/order/:id
+exports.deleteOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("No Order found with this ID", 404));
+    }
+
+    await order.remove();
+
+    res.status(200).json({
+        success: true,
+    });
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,8 +6,8 @@ const {
     getSingleOrder,
     myOrders,
     allOrders,
-    // updateOrder,
-    // deleteOrder
+    updateOrder,
+    deleteOrder
 
 } = require('../controllers/orderController')
 
@@ -19,8 +19,8 @@ router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, myOrders);
 
 router.route('/admin/orders/').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
-// router.route('/admin/order/:id')
-//     .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
-//     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
+router.route('/admin/order/:id')
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
 
 module.exports = router;
