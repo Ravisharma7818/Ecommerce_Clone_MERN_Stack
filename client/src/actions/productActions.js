787ff44bd888151d6fb7c63d6935392b708461b7
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-04-28 19:26:43 +0530

    Product Details Reducer Added

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -4,7 +4,10 @@ import {
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_SUCCESS,
     ALL_PRODUCTS_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 
 } from '../constants/productConstant'
 
@@ -39,4 +42,37 @@ export const clearErros = () => async (dispatch) => {
     dispatch({
         type: CLEAR_ERRORS,
     })
+}
+
+
+export const productDetailsReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload
+            }
+
+        case PRODUCT_DETAILS_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
 }
