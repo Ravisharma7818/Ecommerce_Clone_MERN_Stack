be3164bffdd1b8ee1869b8ef4fdfad65935f56b8
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-05-17 13:38:12 +0530

    Remove File

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ const app = require('./app')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config({ path: 'backend/.env' });
 const cloudinary = require('cloudinary');
-const connectDatabase = require('./config/database')
 
 
 
+async function connectDatabase() {
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
 
+    }).then(con => {
+        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+    })
+}
 connectDatabase();
-
 // Set Cloudinary Configuration 
 
 cloudinary.config({
