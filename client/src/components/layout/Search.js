127288119fa5234051d410b3a1e999ac9fd39f3d
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-06-03 17:52:51 +0530

    Ui Changes

--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -23,7 +23,7 @@ const Search = () => {
                     type="text"
                     id="search_field"
                     className="form-control"
-                    placeholder="Enter Product Name ..."
+                    placeholder="Enter Product Name "
                     onChange={(e) => setKeyword(e.target.value)}
                 />
                 <div className="input-group-append">
